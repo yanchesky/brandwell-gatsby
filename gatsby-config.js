cbe0f6a2675988091e265e196f3fa02347fd3528
Janek Biardzki <jan.biardzki@netinteractive.pl>
2021-07-12 21:51:05 +0200

    ga added

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const {
 const settings = {
   title: "brandwell-gatsby",
   siteUrl: "https://brandwell.pl",
-  //siteUrl: "http://localhost:8000",
+  //siteUrl: "http://localhost:9000",
   defaultLanguage: "pl",
   languages: ["pl", "en"],
   projectsSlug: "portfolio",
@@ -27,15 +27,10 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
